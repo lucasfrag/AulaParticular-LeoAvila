ae09f03dc303e889aefc21bf01b0dc73a447c138
Lucas Fraga <ti.lucasfraga@gmail.com>
2023-08-22 01:43:05 -0300

    Aula 1

---
+++ JavaAnt/src/javaant/JavaAnt.java
@@ -0,0 +1,8 @@
+package javaant;
+
+public class JavaAnt {
+    public static void main(String[] args) {
+        Tela tela1 = new Tela();
+        tela1.setVisible(true);
+    }
+}
---
+++ JavaMaven/src/main/java/com/mycompany/javamaven/JavaMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javamaven;
+
+/**
+ *
+ * @author tiluc
+ */
+public class JavaMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
